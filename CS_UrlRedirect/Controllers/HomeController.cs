ccf4c0822b71298c8762a528fd4b08b11b4b4ff6
keperkjr <keperkjr@yahoo.com>
2022-01-01 19:45:27 -0800

    Added redirect partial view

--- CS_UrlRedirect/Controllers/HomeController.cs
+++ CS_UrlRedirect/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CS_UrlRedirect.Models;
+﻿using CS_UrlRedirect.Data;
+using CS_UrlRedirect.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,10 +13,11 @@ namespace CS_UrlRedirect.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly DatabaseDBContext _context;
+        public HomeController(ILogger<HomeController> logger, DatabaseDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +34,36 @@ namespace CS_UrlRedirect.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // POST: Redirects/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("Id,ShortCode,Url,NumVisits")] RedirectViewModel redirect)
+        {
+            if (string.IsNullOrWhiteSpace(redirect.ShortCode))
+            {
+                ModelState.AddModelError(nameof(redirect.ShortCode), "A short code is required");
+            }
+            if (string.IsNullOrWhiteSpace(redirect.Url))
+            {
+                ModelState.AddModelError(nameof(redirect.Url), "A url is required");
+            }
+
+            if (ModelState.IsValid)
+            {
+                await AddEntry(redirect);
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public async Task AddEntry(Redirect redirect)
+        {
+            _context.Add(redirect);
+            await _context.SaveChangesAsync();
         }
     }
 }
---
+++ CS_UrlRedirect/Models/ViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS_UrlRedirect.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<CS_UrlRedirect.Models.Redirect> redirects { get; set; }
+        public CS_UrlRedirect.Models.RedirectViewModel redirect { get; set; }
+    }
+
+    public class RedirectViewModel : CS_UrlRedirect.Models.Redirect
+    {
+        public enum Action
+        {
+            Create,
+            Update
+        }
+        public Action action = Action.Create;
+    }
+}
