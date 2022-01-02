86c21885c00e5f3580aae18fe7a81d5fc0d374ee
keperkjr <keperkjr@yahoo.com>
2022-01-01 17:45:04 -0800

    Created data folder

--- CS_UrlRedirect/Startup.cs
+++ CS_UrlRedirect/Startup.cs
@@ -28,16 +28,21 @@ namespace CS_UrlRedirect
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string conn = Configuration.GetConnectionString("DefaultConnection");
-            if (conn.Contains("%CONTENTROOTPATH%"))
-            {
-                conn = conn.Replace("%CONTENTROOTPATH%", _contentRootPath);
-            }
+            string connection = UpdateConnectionPath(Configuration.GetConnectionString("DefaultConnection"));
 
-            services.AddDbContext<DatabaseDBContext>(options => options.UseSqlServer(conn));
+            services.AddDbContext<DatabaseDBContext>(options => options.UseSqlServer(connection));
 
             services.AddControllersWithViews();
         }
+
+        private string UpdateConnectionPath(string connection)
+        {
+            if (connection.Contains("%CONTENTROOTPATH%"))
+            {
+                connection = connection.Replace("%CONTENTROOTPATH%", _contentRootPath);
+            }
+            return connection;
+        } 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
